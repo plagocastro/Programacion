5c244872c37f3f6acf4404931c7ac7fed8033a52
plagocastro <plagocastro@danielcastelao.org>
2021-04-21 19:30:45 +0200

    Acabado el ej19

--- 3 EVAL/Boletin19/src/com/PabloLagoCastroPRO/boletin19/Main.java
+++ 3 EVAL/Boletin19/src/com/PabloLagoCastroPRO/boletin19/Main.java
@@ -3,7 +3,7 @@ package com.PabloLagoCastroPRO.boletin19;
 public class Main {
     public static void main(String[] args) {
         ejercicio19 e19 = new ejercicio19();
-        e19.iniciar();
+        e19.crearinterfaz();
 
     }
 }
